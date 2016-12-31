a4a320709a91beae43abfd99179c346f2b37f6f6
Philippe Sabourin <philippe.sabourin@gmail.com>
2016-12-31 14:06:19 -0500

    Extension to provide a way to have nicknames for credit cards on amazon.com

---
+++ extension/src/content.js
@@ -0,0 +1,50 @@
+
+const regex = /ending in\s*(\d{4})/;
+
+let timer = null;
+
+
+function updateNicknames() {
+    chrome.storage.sync.get("nicknames", (data) => {
+        const nicknames = data["nicknames"] == undefined ? {} : data["nicknames"];
+        function appendNickname() {
+            const ccnum = CryptoJS.SHA256($(this).text().match(regex)[1]);
+            if(nicknames[ccnum] != undefined) {
+                $(this).append("<span class='cc_nickname'> - "+nicknames[ccnum]+"</span>");
+            }
+        }
+
+        $("#payment").off("DOMSubtreeModified");
+        $(".cc_nickname").remove();
+        $(".pmts-instrument-number-tail").each(appendNickname);
+        $(".card-info span.a-color-secondary").each(appendNickname);
+        $("#payment-information span.a-color-secondary").each(appendNickname);
+
+        $("#payment").on("DOMSubtreeModified", ()=> {
+            if (timer != null) {
+                clearTimeout(timer);
+            }
+            timer = setTimeout(updateNicknames,250);
+        });
+    });
+}
+
+function getCardNumbers(type) {
+    let cardNumbers = [];
+    let selector = null;
+    if (type == 1) {
+        selector = $(".pmts-instrument-number-tail");
+    } else if (type == 2) {
+        selector = $(".card-info span.a-color-secondary");
+    }
+   selector.each(function(){
+        cardNumbers.push($(this).text().match(regex)[1]);
+    });
+
+    chrome.runtime.sendMessage({
+        action: "getCards",
+        numbers: cardNumbers
+    });
+}
+
+updateNicknames();
---
+++ extension/src/popup.js
@@ -0,0 +1,120 @@
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Get the current URL.
+ *
+ * @param {function(string)} callback - called when the URL of the current tab
+ *   is found.
+ */
+function getCurrentTabUrl(callback) {
+    // Query filter to be passed to chrome.tabs.query - see
+    // https://developer.chrome.com/extensions/tabs#method-query
+    const queryInfo = {
+        active: true,
+        currentWindow: true
+    };
+
+    chrome.tabs.query(queryInfo, function (tabs) {
+        // chrome.tabs.query invokes the callback with a list of tabs that match the
+        // query. When the popup is opened, there is certainly a window and at least
+        // one tab, so we can safely assume that |tabs| is a non-empty array.
+        // A window can only have one active tab at a time, so the array consists of
+        // exactly one tab.
+        const tab = tabs[0];
+
+        // A tab is a plain object that provides information about the tab.
+        // See https://developer.chrome.com/extensions/tabs#type-Tab
+        const url = tab.url;
+
+        // tab.url is only available if the "activeTab" permission is declared.
+        // If you want to see the URL of other tabs (e.g. after removing active:true
+        // from |queryInfo|), then the "tabs" permission is required to see their
+        // "url" properties.
+        console.assert(typeof url == 'string', 'tab.url should be a string');
+
+        callback(url);
+    });
+
+    // Most methods of the Chrome extension APIs are asynchronous. This means that
+    // you CANNOT do something like this:
+    //
+    // var url;
+    // chrome.tabs.query(queryInfo, function(tabs) {
+    //   url = tabs[0].url;
+    // });
+    // alert(url); // Shows "undefined", because chrome.tabs.query is async.
+}
+
+function renderStatus(statusText) {
+    document.getElementById('status').textContent = statusText;
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender) {
+    if (request.action == "getCards") {
+        if (request.numbers.length) {
+            let nicknames = {};
+            chrome.storage.sync.get("nicknames", (data) => {
+                if(data["nicknames"] != undefined) {
+                    nicknames = data["nicknames"];
+                }
+                renderStatus("Cards and nicknames:");
+                request.numbers.forEach((item) => {
+                    const ccHash = CryptoJS.SHA256(item);
+                    const oldValue = (nicknames[ccHash] != undefined) ? nicknames[ccHash] : "";
+                    const input = $("<input type='text' id='card"+ccHash+"' value='"+oldValue+"' placeholder='Nickname'/>");
+                    let node = $("<div><span>"+item+"</span> </div>").append(input);
+                    input.change(()=>{
+                        nicknames[ccHash] = input.val();
+                       chrome.storage.sync.set({"nicknames": nicknames}, () => {
+                           console.log("Saved nicknames.");
+                           chrome.tabs.executeScript(null, {
+                               code: "updateNicknames();"
+                           }, function() {
+                               // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+                               if (chrome.runtime.lastError) {
+                                   message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+                               }
+                           });
+                       });
+                    });
+                    $("#cardNicknames").append(node);
+                });
+                $("#cardNicknames").show();
+            });
+
+        } else {
+            renderStatus("No cards found on the page.");
+        }
+    }
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    getCurrentTabUrl(function (url) {
+        let type = 0;
+
+        if (url.startsWith("https://www.amazon.com/gp/wallet")) {
+            type = 1;
+        } else if (url.startsWith("https://www.amazon.com/gp/buy/") ) {
+            type = 2;
+        }
+
+        if (type > 0) {
+            renderStatus("This is an amazon wallet tab.");
+            chrome.tabs.executeScript(null, {
+                code: "getCardNumbers("+type+");"
+            }, function() {
+                // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+                if (chrome.runtime.lastError) {
+                    message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+                }
+            });
+        } else {
+            renderStatus("This is not an amazon wallet or payment tab.");
+        }
+    });
+});
+
+
